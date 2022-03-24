44d378029a6fd4dec6598c306f820c6cebc1e2c2
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-24 17:26:47 +0100

    refactor with custom hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,20 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-import youtube from "../apis/youtube";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, onSearch] = useVideos("hippo");
 
+  //Cada vez que se recibe unha nova lista de vídeos, selecciónase o primeiro
   useEffect(() => {
-    onTermSubmit("hippo");
-  }, []);
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="app-container ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={onSearch} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
